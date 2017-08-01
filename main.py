16382fa20614d7de1b4e3b38102e7463c16514c8
Viral Joshi <viralj@users.noreply.github.com>
2017-08-01 09:40:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,33 @@ DATABASE_NAME = "news"
 
 # Database query function
 def query_db(query):
-    ''' Makes a query to the news database '''
+    """ Makes a query to the news database """
     try:
         db = psycopg2.connect(database=DATABASE_NAME)
         c = db.cursor()
         c.execute(query)
+        a = c.fetchall()
         db.close()
-        return c.fetchall()
+        return a
     except Exception:
         return None
 
+
 def three_most_popular_articles():
-    '''1. What are the most popular three articles of all time?
+    """1. What are the most popular three articles of all time?
     Which articles have been accessed the most?
     Present this information as a sorted list with
-    the most popular article at the top.'''
-    
+    the most popular article at the top."""
+
     # To print information
     information_string = '1. The 3 most popular articles of all time are:\n'
 
     # Query string
     query = """select title,count(*) as num from
-             articles,log where
-             log.path=CONCAT('/article/',articles.slug)
-             group by articles.title
-             order by num DESC limit 3;"""
+                articles,log where
+                 log.path=CONCAT('/article/',articles.slug)
+                  group by articles.title
+                   order by num DESC limit 3;"""
 
     print(information_string)
     for result in query_db(query):
@@ -38,13 +40,14 @@ def three_most_popular_articles():
 
     print("\n")
 
+
 def most_popular_article_authors():
-    '''2. Who are the most popular article authors of all time?
+    """2. Who are the most popular article authors of all time?
     That is, when you sum up all of the articles each author
     has written, which authors get the most page views?
     Present this as a sorted list with the most popular
-    author at the top.'''
-    
+    author at the top."""
+
     # To print information
     information_string = '2. The most popular article ' \
                          'authors of all time are:\n'
@@ -62,24 +65,25 @@ def most_popular_article_authors():
 
     print("\n")
 
+
 def days_with_request():
-    '''3. On which days did more than 1% of requests
+    """3. On which days did more than 1% of requests
     lead to errors? The log table includes a column
     status that indicates the HTTP status code that
     the news site sent to the user's browser.
     (Refer back to this lesson if you want to
-    review the idea of HTTP status codes.)'''
-    
+    review the idea of HTTP status codes.)"""
+
     # To print information
     information_string = '3. Days with more than ' \
                          '1% of request that lead to an error:\n'
 
     # Query string
     query = """select * from (select date(time),
-            round(100.0*sum(case log.status 
-            when '200 OK'  then 0 else 1 end)/count(log.status),3)
+            round(100.0*sum(case log.status
+             when '200 OK'  then 0 else 1 end)/count(log.status),3)
              as error from log group by date(time)
-            order by error desc) as subq where error > 1;"""
+             order by error desc) as subq where error > 1;"""
 
     print(information_string)
     for result in query_db(query):
